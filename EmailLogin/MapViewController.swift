a4e6dbefe4ca5ae5eef912650be660e9b9cc1437
Basil <basiltalias92@gmail.com>
2016-12-06 23:33:47 -0500

    Finalize the working product

--- EmailLogin/MapViewController.swift
+++ EmailLogin/MapViewController.swift
@@ -19,7 +19,7 @@ class MapViewController :UIViewController,MKMapViewDelegate,CLLocationManagerDel
     
     let locationManager = CLLocationManager()
     let databaseRef = FIRDatabase.database().reference()
-    let annotation = MKPointAnnotation()
+   // let annotation = MKPointAnnotation()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,8 +59,8 @@ class MapViewController :UIViewController,MKMapViewDelegate,CLLocationManagerDel
         annotation.coordinate = location
         annotation.title = "Pizza Store"
         annotation.subtitle = "Toronto"
-        
-        mapView.addAnnotation(annotation) */
+        */
+        //mapView.addAnnotation(annotation)
     
         self.locationManager.delegate = self
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
@@ -76,9 +76,10 @@ class MapViewController :UIViewController,MKMapViewDelegate,CLLocationManagerDel
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations.last
         post(location: location!)
-        let center = CLLocationCoordinate2D(latitude: (location?.coordinate.latitude)!, longitude: (location?.coordinate.longitude)!)
         
-        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+    /*    var center = CLLocationCoordinate2D(latitude: (location?.coordinate.latitude)!, longitude: (location?.coordinate.longitude)!)
+        
+        var region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1.0, longitudeDelta: 1.0))
         self.mapView.setRegion(region, animated: true)
         
         mapView.removeAnnotation(annotation)
@@ -86,15 +87,63 @@ class MapViewController :UIViewController,MKMapViewDelegate,CLLocationManagerDel
         annotation.title = FIRAuth.auth()?.currentUser?.email
         annotation.subtitle = "Toronto"
         self.mapView.addAnnotation(annotation)
-        
-        databaseRef.child("Locations").queryOrderedByKey().observe(.childAdded, with: {
+        */
+       /* databaseRef.child("Locations").queryOrderedByKey().observe(.childAdded, with: {
             snapshot in
             
             let email = (snapshot.value as? NSDictionary)?["Email"] as? String ?? ""
             print(email)
             
-        })
+        })*/
+      /*  databaseRef.child("Locations").observeSingleEvent(of:.childChanged, with: {
+            snapshot in
+            
+            let email = (snapshot.value as? NSDictionary)?["Email"] as? String ?? ""
+            if email != FIRAuth.auth()?.currentUser?.email {
+                print(email)
+                /*center = CLLocationCoordinate2D(latitude: (location?.coordinate.latitude)!, longitude: (location?.coordinate.longitude)!)
+                
+                region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+                self.mapView.setRegion(region, animated: true)*/
+                let lat = (snapshot.value as? NSDictionary)?["Latitude"] as? String ?? ""
+                let lon = (snapshot.value as? NSDictionary)?["Longitude"] as? String ?? ""
+                print("Latitude: \(lat) & Longitude: \(lon)")
+
+            }
+        })*/
         
+        databaseRef.child("Locations").observeSingleEvent(of: .value, with: { snapshot in
+            
+           
+            if snapshot.value is NSNull {
+                
+            }else {
+                let snapDic = snapshot.value as? NSDictionary
+                for child in snapDic! {
+                let childDic = child.value as? NSDictionary
+                let email = childDic?["Email"] as! String
+                let lat = childDic?["Latitude"] as! NSNumber
+                let lon = childDic?["Longitude"] as! NSNumber
+                let lastTime = childDic?["Timestamp"] as! NSNumber
+                    
+                print("Email:\(email) & Location: \(lat) + \(lon)")
+             /*   var center = CLLocationCoordinate2D(latitude: CLLocationDegrees(lat), longitude: CLLocationDegrees(lon))
+                var region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1.0, longitudeDelta: 1.0))
+                self.mapView.setRegion(region, animated: true) */
+                let annotation = MKPointAnnotation()
+                self.mapView.removeAnnotation(annotation)
+                
+                annotation.title = email
+                let dateTime = String(describing: lastTime)
+                annotation.subtitle = "Last Updated: "+dateTime
+                annotation.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(lat), longitude: CLLocationDegrees(lon))
+                self.mapView.addAnnotation(annotation)
+                
+                
+                }
+            }
+            
+        })
         self.locationManager.startUpdatingLocation()
     }
     
